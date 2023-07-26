f422678e67fcae7d68fbb206a7a969d62be17e1b
Alex <Alex@ALEXSLITTLEWARM>
2023-07-26 13:07:24 +0300

    Switch de dark mode fara tutorial

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,44 +1,35 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline, Typography } from "@mui/material";
+import { Container, CssBaseline, Switch, createTheme } from "@mui/material";
 import Header from "./Header";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
 
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-useEffect(() => {
-  fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-}, [])
-
-
-  function addProduct(){
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1), 
-        price: (prevState.length * 100) + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-      }])
+  const [darkMode, setDarkMode] = useState(false);
+  const palletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette:{
+      mode: palletteType
     }
-
+  })
+  function SwitchDarkMode(){
+    setDarkMode(!darkMode);
+  }
 
   return (
-    <>
-    <CssBaseline/>
-      <Header/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header switchDarkMode={SwitchDarkMode}/>
       <Container>
-          <Catalog products = {products} addProduct = {addProduct}/>
+        <Catalog/>
       </Container>
-      
-    </>
+
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,12 +1,21 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ChangeEvent } from "react";
 
-export default function Header(){
+interface Props{
+    switchDarkMode: () => void;
+}
+
+export default function Header({switchDarkMode}: Props){
+    const handleSwitchChange = () => {
+        switchDarkMode();
+    }
     return(
         <AppBar position = "static" sx = {{mb: 4}}>
             <Toolbar>
                 <Typography variant='h6'>
                     RE-STORE
                 </Typography>
+                <Switch onChange={handleSwitchChange}/>
             </Toolbar>
         </AppBar>
     )
