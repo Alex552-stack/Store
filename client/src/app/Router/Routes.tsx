ddcf69e8d9ddbf399e4f102560d410253c66988d
Alex <Alex@ALEXSLITTLEWARM>
2023-07-26 16:00:58 +0300

    End of section 4, react routing

---
+++ client/src/app/Router/Routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../layout/App";
+import HomePage from "../../features/home/HomePage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetailes from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <App />,
+            children: [
+                {path: '', element: <HomePage/>},
+                {path: 'catalog', element: <Catalog/>},
+                {path: 'catalog/:id', element: <ProductDetailes/>},
+                {path: 'about', element: <AboutPage/>},
+                {path: 'contact', element: <ContactPage/>}
+            ]
+        }
+    ]
+)
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import { Container, CssBaseline, Switch, createTheme } from "@mui/material";
 import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 
 
@@ -26,7 +27,7 @@ function App() {
       <CssBaseline />
       <Header switchDarkMode={SwitchDarkMode}/>
       <Container>
-        <Catalog/>
+        <Outlet/>
       </Container>
 
     </ThemeProvider>
