7ce279054ff06dea5951bbb74941f336533ebfd8
Alex <Alex@ALEXSLITTLEWARM>
2023-08-01 16:26:31 +0300

    Some sort of pagination error

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,14 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class ProductsController:BaseApiController
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
 
@@ -15,16 +18,37 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
-            return  await _context.Products.ToListAsync();
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
+        {
+            var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagesList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id){
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
             var product = await _context.Products.FindAsync(id);
-            
-            if(product == null) return NotFound();
-            
+
+            if (product == null) return NotFound();
+
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
         }
 
     }
--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../Router/Routes";
+import { PaginatedResponse } from "../models/pagination";
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 axios.defaults.withCredentials = true;
 
@@ -10,15 +11,21 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(async response => {
     await sleep();
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+        console.log(response.data);
+        return response;
+    }
     return response;
 }, (error: AxiosError) => {
     const { data, status } = error.response as AxiosResponse;
     switch (status) {
         case 400:
-            if(data.errors){
+            if (data.errors) {
                 const modelStateErrors: string[] = [];
-                for(const key in data.errors){
-                    if(data.errors[key]){
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
                         modelStateErrors.push(data.errors[key])
                     }
                 }
@@ -30,7 +37,7 @@ axios.interceptors.response.use(async response => {
             toast.error(data.title);
             break;
         case 500:
-            router.navigate('/server-error', {state: {error: data}});
+            router.navigate('/server-error', { state: { error: data } });
             break;
         default:
             break
@@ -39,15 +46,16 @@ axios.interceptors.response.use(async response => {
 });
 
 const request = {
-    get: (url: string) => axios.get(url).then(responseBody),
+    get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody)
 }
 
 const Catalog = {
-    list: () => request.get('products'),
-    details: (id: number) => request.get(`products/${id}`)
+    list: (params: URLSearchParams) => request.get('products', params),
+    details: (id: number) => request.get(`products/${id}`),
+    fetchFilters: () => request.get('products/filters')
 }
 
 const TestErrors = {
